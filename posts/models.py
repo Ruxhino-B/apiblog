30e8ba46197de13e6e7d63043337932b4d1e9cdb
ruxhino <ruxhino.balliu@fshnstudent.info>
2021-05-12 03:41:43 +0200

    First Commit

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from users.models import CustomUser
+# Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
